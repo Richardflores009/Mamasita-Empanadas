c3b407ffa2b960a5457cbe3ca04bee739970c469
T Brichetto <TaraPersonal@MacBook-Air.local>
2021-01-20 14:30:01 -0700

    contact form alert

--- client/src/components/ContactForm/index.js
+++ client/src/components/ContactForm/index.js
@@ -2,10 +2,14 @@ import React, { Component } from "react";
 
 class ContactForm extends Component {
     render () {
+      const handleSubmit = () => {
+        alert("Thank you for reaching out! Mamasita's is not up and running just yet, but we look forward to hearing from you in the future!");
+    }
+
     return (
         <div id="contact-form">
           <p>Note: Please use the menu feature or call us directly to place an order. </p>
-          <form>
+          <form onSubmit={handleSubmit}>
             <div className="contact-form-row">
               <label for="name">Your Name:</label>
               <input type="text" id="name" required />
